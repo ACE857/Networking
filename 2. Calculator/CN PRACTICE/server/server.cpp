b2e381184c1a073daf1886ab53668a0e79fc8d2e
ACE857 <srjbsht857@gmail.com>
2018-10-31 16:34:45 +0530

    Update server.cpp

--- 2. Calculator/CN PRACTICE/server/server.cpp
+++ 2. Calculator/CN PRACTICE/server/server.cpp
@@ -1,101 +1,84 @@
 #include<bits/stdc++.h>
-#include<arpa/inet.h>
 #include<sys/socket.h>
-
+#include<arpa/inet.h>
 using namespace std;
 
 class ace{
 	public :
-	int sock,a,choice,cli,sizeServ;
-	string menu;
-	double angle;
-	int op;
-	struct sockaddr_in serv;
+		int serv,cli;
+		struct sockaddr_in s;
 		ace(){
-		menu = "\nTRIGNOMETRIC CALCULATOR\n1.SIN\n2.COS\n3.TAN\n";
-		angle=0;
-		op=-1;
-			sock = socket(PF_INET,SOCK_STREAM,0);
-			serv.sin_family = AF_INET;
-			serv.sin_port = htons(9965);
-			serv.sin_addr.s_addr = htonl(INADDR_ANY);
-			if(bind(sock,(struct sockaddr *)&serv,sizeof(serv))<0){
+			serv = socket(PF_INET,SOCK_STREAM,0);
+			s.sin_family = AF_INET;
+			s.sin_port = htons(9998);
+			s.sin_addr.s_addr = htonl(INADDR_ANY);
+			cout<<"\nSocket Created Sucessfully\n";
+			if(bind(serv,(struct sockaddr *)&s,sizeof(s))<0){
 				cout<<"\nBind Failed\n";
-				exit(0);
 			}
-			cout<<"\nBind Succesfull";
-			if(listen(sock,3)<0){
-				cout<<"\nListining Failed\n";
-				exit(0);
+			cout<<"\nBind Sucessfull\n";
+			if(listen(serv,3)<0){
+				cout<<"\nListen Failed\n";
 			}
-			cout<<"\nListining for clients\n";
-			sizeServ = sizeof(serv);
-			cli = accept(sock,(struct sockaddr *)&serv,(socklen_t *)&sizeServ);
+			cout<<"\nListining for client o.0.o.0.o.......\n";
+			socklen_t slen = sizeof(s);
+			cli = accept(serv,(struct sockaddr *)&s,(socklen_t *)&slen);
 			if(cli<0){
-				cout<<"\nClinet Connection Failed\n";
-				exit(0);
+				cout<<"\nConnect Failed\n";
 			}
 			cout<<"\nClient Connected\n";
 			
-		}
-		void sndDouble(double *s){
-			if(send(cli,s,sizeof(double),0)<0){
-				cout<<"\nSend Failed : "<<s<<endl;
-				exit(0);
-			}
 			
 		}
 		
-		void snd(string s){
-			int len = s.length();
-			if(send(cli,&s[0],len,0)<0){
-				cout<<"\nSend Failed : "<<s<<endl;
-				exit(0);
-			}
-			
-		}
-		void recvInt(int *r){
-			if(recv(cli,r,sizeof(int),0)<0){
-				cout<<"\nReciving Failed\n";
-			}
-		}
-		void recvDou(double *r){
-			if(recv(cli,r,sizeof(double),0)<0){
-				cout<<"\nReciving Failed\n";
+		void snd(){
+			char msg[50]={0};
+			cout<<"\nEnter message : ";	cin>>msg;
+			int l = strlen(msg);
+			if(send(cli,&msg[0],l,0)<0){
+				cout<<"\nSend Failed\n";
 			}
 		}
 		
-		double calculate(int choi,double angle){
-		int i,j,k;
-		double result=0;
-		cout<<"\nChoice is "<<choi<<"  "<<"angle is  "<<angle;
-			switch(choi){
-				case 1 : cout<<choi; angle = angle*3.14/180;
-			result = sin(angle);	break;
-				case 2 : cout<<choi; angle = angle*3.14/180;
-			result = cos(angle);	break;
-				case 3 : cout<<choi; angle = angle*3.14/180;
-			result = tan(angle);	break;
+		void rec(){
+			char msg[50]={0};
+			if(recv(cli,&msg[0],50,0)<0){
+				cout<<"\nReceving Failed\n";
 			}
-		return result;
+			cout<<msg<<endl;
 		}
-		
-		void cl(){
-			shutdown(sock,2);
+		void close(){
 			shutdown(cli,2);
+			shutdown(serv,2);
 		}
+		void serve(){
+			string str = "1.SIN\n2.COS\n3.TAN\n";	
+			if(send(cli,&str[0],str.length(),0)<0){
+				cout<<"\nSend Failed\n";
+			}
+			int ch=0;
+			if(recv(cli,&ch,sizeof(ch),0)<0){
+				cout<<"\nReceving Failed\n";
+			}
+			cout<<"\nChoice Is "<<ch;
+			double ang;
+			if(recv(cli,&ang,sizeof(ang),0)<0){
+				cout<<"\nReceving Failed\n";
+			}
+			cout<<"\nAngle Is "<<ang<<endl;
+			double rt = tan((3.14*ang)/180);
+			
+			if(send(cli,&rt,sizeof(rt),0)<0){
+				cout<<"\nSend Failed\n";
+			}
+			
+		}
+
 };
 
-
-int main()
-{
+int main(){
 	ace a;
-	a.snd(a.menu);
-	a.recvInt(&(a.choice));
-	a.recvDou(&(a.angle));
-	double ans = a.calculate(a.choice,a.angle);
-	a.sndDouble(&ans);
-	cout<<"\nAns Calculated and Send\n"<<ans;
-	a.cl();
-	
+	int i=3;
+	a.serve();
+	a.close();
 }
