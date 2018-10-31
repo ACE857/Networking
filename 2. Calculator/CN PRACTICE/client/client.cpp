299287e3cd858772809605596e1a2e92e938ccc6
ACE857 <srjbsht857@gmail.com>
2018-10-31 16:33:57 +0530

    Update client.cpp

--- 2. Calculator/CN PRACTICE/client/client.cpp
+++ 2. Calculator/CN PRACTICE/client/client.cpp
@@ -1,88 +1,73 @@
 #include<bits/stdc++.h>
-#include<sys/socket.h>
 #include<arpa/inet.h>
-
+#include<sys/socket.h>
 using namespace std;
 
 class ace{
 	public :
-	int cli,a,b,c;
-	struct sockaddr_in client;
+		int serv;
+		struct sockaddr_in s;
 		ace(){
-			cli = socket(PF_INET,SOCK_STREAM,0);
-			client.sin_family = AF_INET;
-			client.sin_port = htons(9965);
-			client.sin_addr.s_addr = inet_addr("127.0.0.1");
-			if(connect(cli,(struct sockaddr *)&client,sizeof(client))<0){
-				cout<<"\nConnection Failed\n";
-				exit(0);
-			}
-			cout<<"\nConected OK\n";
-		
-		}
-		
-		void snd(string s){
-			int len = s.length();
-			if(send(cli,&s[0],len,0)<0){
-				cout<<"\nSend Failed : "<<s<<endl;
-				exit(0);
-			}
+			serv = socket(PF_INET,SOCK_STREAM,0);
+			cout<<"\nSocket Created\n";
+			s.sin_family = AF_INET;
+			s.sin_port = htons(9998);
+			s.sin_addr.s_addr = inet_addr("127.0.0.1");
 			
-		}
-		void sndInt(int *s){
-			if(send(cli,s,sizeof(int),0)<0){
-				cout<<"\nSend Failed : "<<s<<endl;
-				exit(0);
+			if(connect(serv,(struct sockaddr *)&s,sizeof(s))<0){
+				cout<<"\nUnable To Connect\n";
 			}
-			
-		}
-		void sndDouble(double *s){
-			if(send(cli,s,sizeof(double),0)<0){
-				cout<<"\nSend Failed : "<<s<<endl;
-				exit(0);
-			}
+			cout<<"\nConnection Established\n";
 			
 		}
 		
-		void recvString(char *ch){
-			if(recv(cli,ch,50,0)<0){
-				cout<<"\nReciving Failed\n";
+		void snd(){
+			char msg[50]={0};
+			cout<<"\nEnter Message : ";	cin>>msg;
+			int i,l=strlen(msg);
+			if(send(serv,&msg[0],l,0)<0){
+				cout<<"\nSend Failed\n";
 			}
 		}
 		
-		
-		void recvInt(int *r){
-			if(recv(cli,r,sizeof(int),0)<0){
-				cout<<"\nReciving Failed\n";
+		void rec(){
+			char msg[50]={0};
+			if(recv(serv,&msg[0],50,0)<0){
+				cout<<"\nReceive Failed\n";
 			}
+			cout<<msg<<endl;
 		}
-		void recvDou(double *r){
-			if(recv(cli,r,sizeof(double),0)<0){
-				cout<<"\nReciving Failed\n";
+		void close(){
+			shutdown(serv,2);
+		}
+		void serve(){
+			char msg[50]={0};
+			if(recv(serv,&msg[0],50,0)<0){
+				cout<<"\nReceive Failed\n";
 			}
-		}
-		void cl(){
-			shutdown(cli,2);
+			cout<<msg<<endl;
+			int ch=0;
+			cin>>ch;
+			if(send(serv,&ch,sizeof(ch),0)<0){
+				cout<<"\nSend Failed\n";
+			}
+			double ang = 0.0;
+			cout<<"\nEnter Angle  :  ";	cin>>ang;
+			if(send(serv,&ang,sizeof(ang),0)<0){
+				cout<<"\nSend Failed\n";
+			}
+			if(recv(serv,&ang,sizeof(ang),0)<0){
+				cout<<"\nReceving Failed\n";
+			}
+			cout<<"\nAns Is "<<ang<<endl;
 			
 		}
-		
 };
 
-
-int main()
-{
+int main(){
 	ace a;
-	char ch[50];
-	a.recvString(ch);
-	cout<<ch<<endl;
-	int choice,op,i,j,k,l;
-	double angle,ans=0;
-	cin>>choice;
-	a.sndInt(&choice);
-	cout<<"\nEnter Angle  ";
-	cin>>angle;
-	a.sndDouble(&angle);
-	a.recvDou(&ans);
-	cout<<"\nAns Is"<<ans;
-	a.cl();
+	int i=3;
+	a.serve();
+	a.close();
+	
 }
