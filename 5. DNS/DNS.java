590d4d08e76e6cacfedf0554a18861812ca99cf2
ACE857 <srjbsht857@gmail.com>
2018-10-31 15:19:30 +0530

    Update DNS.java

--- 5. DNS/DNS.java
+++ 5. DNS/DNS.java
@@ -1,44 +1,62 @@
-import javax.naming.directory.Attributes;
-import javax.naming.directory.InitialDirContext;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+// Print out DNS Record for an Internet Address
+import javax.naming.directory.*;
+import javax.naming.*;
+import java.net.*;
 import java.util.Scanner;
+
 public class DNS
 {
     public static void main(String args[])
     {
-        
-        String url;
-        System.out.print("\nEnter URL of Website : ");
-        Scanner sc = new Scanner(System.in);
-        url = sc.next();
+         // explain what program does and how to use it 
+        if (args.length != 1)
+        {
+            System.err.println("Print out DNS Record for an Internet Address");
+            System.err.println("USAGE: java DNSLookup domainName|domainAddress");
+            System.exit(-1);
+        }
         try
         {
             InetAddress inetAddress;
-            if (Character.isDigit(url.charAt(0)))
-            {   
+            // if first character is a digit then assume is an address
+            if (Character.isDigit(args[0].charAt(0)))
+            {   // convert address from string representation to byte array
                 byte[] b = new byte[4];
-                String[] bytes = url.split("[.]");
+                String[] bytes = args[0].split("[.]");
                 for (int i = 0; i < bytes.length; i++)
                 {
                     b[i] = new Integer(bytes[i]).byteValue();
                 }
-                
+                // get Internet Address of this host address
                 inetAddress = InetAddress.getByAddress(b);
             }
             else
-            {   
-                inetAddress = InetAddress.getByName(url);
+            {   // get Internet Address of this host name
+                inetAddress = InetAddress.getByName(args[0]);
             }
-
-            System.out.println(inetAddress.getHostName() + " - " + inetAddress.getHostAddress());
-            
+            // show the Internet Address as name/address
+            System.out.println(inetAddress.getHostName() + "/" + inetAddress.getHostAddress());
+            // get the default initial Directory Context
+            InitialDirContext iDirC = new InitialDirContext();
+            // get the DNS records for inetAddress
+            Attributes attributes = iDirC.getAttributes("dns:/" + inetAddress.getHostName());
+            // get an enumeration of the attributes and print them out
+            NamingEnumeration attributeEnumeration = attributes.getAll();
+            System.out.println("-- DNS INFORMATION --");
+            while (attributeEnumeration.hasMore())
+            {
+                System.out.println("" + attributeEnumeration.next());
+            }
+            attributeEnumeration.close();
         }
         catch (UnknownHostException exception)
         {
-            System.err.println(" ");
+            System.err.println("ERROR: No Internet Address for '" + args[0] + "'");
         }
+        catch (NamingException exception)
+        {
+            System.err.println("ERROR: No DNS record for '" + args[0] + "'");
+        }
+    
     }
 }
